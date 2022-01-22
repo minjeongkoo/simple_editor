fd9ddafcfd81e12cb6c3fa8b624ba04811a14a0d
minjeongKoo <koominjeong99@gmail.com>
2022-01-22 11:02:06 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/Editor">Text Editor</router-link>
+      <router-link to="/about">Database</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Poppins', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+body, #app {
+  background-color: #fafafa;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  margin: 12px;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+a {
+  text-decoration: none;
+}
+
+</style>
---
+++ src/views/Editor.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <!-- Section Controll -->
+    <div class="section-controll">
+      <span>Text Align</span>
+      <button>Left</button>
+      <button>Center</button>
+      <button>Right</button>
+
+      <span>Add</span>
+      <button>HeadLine</button>
+      <button>Caption</button>
+      <button></button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+button {
+  text-align: center;
+  padding: 0.5rem 1rem;
+  margin: 0.2rem;
+  border: 1px solid rgba(69, 90, 100, 0.5);
+  border-radius: 4px;
+  font-size: 1rem;
+  color: #455a64;
+  background: rgba(69, 90, 100, 0.05);
+}
+
+button:hover {
+  background: rgba(69, 90, 100, 0.16);
+}
+
+.section-controll {
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+  background: #fff;
+  box-shadow: 0px 4px 1rem rgba(69, 90, 100, 0.16);
+}
+
+.section-controll>span {
+  margin-right: 1rem;
+  margin-left: 1.5rem;
+  color: #2c3e50;
+  opacity: 0.64;
+}
+
+</style>
